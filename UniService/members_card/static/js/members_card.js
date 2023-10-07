b9257c683e8cc736654f6a3cc26412c4d69bb168
Sharonvdb <133656319+Sharonvdb@users.noreply.github.com>
2023-10-07 23:17:01 +1100

    Can get the filename but cant upload the file. Not sure of the full path

--- UniService/members_card/static/js/members_card.js
+++ UniService/members_card/static/js/members_card.js
@@ -18,6 +18,17 @@ async function limit_input() {
             .catch((err) => {
                 alert(err);
             })
+            const usercard = await fetch(`/api/card/${1}`) //TODO Not all the members are in the cards database for looking up expirary date
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw new Error(`Not a valid Member ID: ${sid}`)
+                }
+            })
+            .catch((err) => {
+                alert(err);
+            })
         //Fill the text fields with a fetch here
         var FName = document.getElementById("txtFName");
         FName.value = user.first_name;
@@ -28,26 +39,36 @@ async function limit_input() {
             user.first_name,
             user.last_name,
             user.image,
-            user.dob
+            user.dob,
+            usercard.issued_date,
+            usercard.expiry_date
         );
     }
     var counterSpan = document.getElementById("counterHolder");
     counterSpan.innerHTML = iEntered;
 }
 
-function ChangePic(oFileInput) {
-    pic = oFileInput.value;
+function ChangePic(income) {
+    var fileupload = document.getElementById("FileUpload");
+    var filePath = document.getElementById("spnFilePath");
+    var button = document.getElementById("btnChoose");
+    //var fileName = income.value;
+    var fileName = income.value.split('\\')[income.value.split('\\').length - 1];
+    filePath.innerHTML = "<b>Selected File: </b>" + fileName;
     showSelection();
+    document.getElementById("photo").src = fileName;
 }
+    
 
-function showSelection(sid, firstname, lastname, img, dob) {
+function showSelection(sid, firstname, lastname, img, dob, issue, expire) {
     document.getElementById("cardmember").innerText = sid;
     document.getElementById("cardname").innerText = `${firstname} ${lastname}`;
-    document.getElementById("cardex").innerText = "13/12/23";
+    //document.getElementById("cardex").innerText = "13/12/23";
+    document.getElementById("cardex").innerHTML = `${issue} to ${expire}`;
     // document.getElementById("dob") = dob; // Todo: Not working.
     // deepcode ignore DOMXSS: This is validated elsewhere.
     document.getElementById("inputdata").innerHTML = DrawCode39Barcode(String(sid), 0);
+    document.getElementById("overlay").src="../accounts/static/images/sample_stamp.png"
     document.getElementById("photo").src = img;
-    document.getElementById("HEREIAM")
 }
 // limit_input()
